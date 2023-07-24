ecf47bc75a97121573f3fa06e0a2014261a829e5
Abdullah149081 <masudmd425@gmail.com>
2023-07-24 13:03:04 +0600

    error page done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Main from "../Layout/Main";
+import ErrorPage from "../components/ErrorPgae/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    // errorElement: <ErrorPage />,
+    errorElement: <ErrorPage />,
 
     children: [
       {
